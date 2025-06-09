4e23278d8ddebc1699143b3161a866e61ad894f3
UtsavBalar1231 <utsavbalar1231@gmail.com>
2025-06-09 10:51:14 +0000

    drivers: clk: Fix unused-variable warning
    ../drivers/clk/clk.c:56:27: warning: unused variable 'all_lists' [-Wunused-variable]
    static struct hlist_head *all_lists[] = {
                              ^
    
    Change-Id: I3af97e5a48ee5e1c456c6b8457a1d1df95a26cd6

--- drivers/clk/clk.c
+++ drivers/clk/clk.c
@@ -2700,6 +2700,12 @@ static u32 debug_suspend;
 static DEFINE_MUTEX(clk_debug_lock);
 static HLIST_HEAD(clk_debug_list);
 
+static struct hlist_head *all_lists[] = {
+	&clk_root_list,
+	&clk_orphan_list,
+	NULL,
+};
+
 static struct hlist_head *orphan_list[] = {
 	&clk_orphan_list,
 	NULL,
